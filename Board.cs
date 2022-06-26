baaef3896172623078faa57a45c0d402175e5a05
JooEun-Jeong <prichard147@naver.com>
2022-06-27 01:25:47 +0900

    Inflearn 강의_ part2 미로구현 (아직 오른손 법칙은 안함)

--- Board.cs
+++ Board.cs
@@ -7,9 +7,13 @@ namespace Maze
     class Board
     {
         // public int[] _data = new int[25];
-        public TileType[,] _tile;
-        public int _size;
+        public TileType[,] Tile { get; private set; }
+        public int size { get; private set; }
         const char CIRCLE = '\u25cf';
+        public int DestY { get; private set; }
+        public int DestX { get; private set; }
+
+        Player _player;
 
         // enum: 상수 숫자를 좀더 의미있게 사용하기 위함
         public enum TileType
@@ -18,39 +22,45 @@ namespace Maze
             Wall
         }
 
-        public void Initialize(int size)
+        public void Initialize(int size, Player player)
         {
-            _tile = new TileType[size, size];
-            _size = size;
+            if (size % 2 == 0)
+                return;
+            Tile = new TileType[size, size];
+            this.size = size;
+            _player = player;
 
-            //generateBinaryTree();
-            generateSideWinder();
+            DestY = size - 2;
+            DestX = size - 2;
+
+            generateBinaryTree();
+            //generateSideWinder();
         }
 
         void generateSideWinder() {  // sidewinder 미로 생성 알고리즘 참고.
 
             // 테두리 및 짝수번째 막아버리는 작업
-            for (int i = 0; i < _size; i++)
+            for (int i = 0; i < size; i++)
             {
-                for (int j = 0; j < _size; j++)
+                for (int j = 0; j < size; j++)
                 {
-                    if (i == 0 || i == _size - 1 || j == 0 || j == _size - 1 || i % 2 == 0 || j % 2 == 0)
+                    if (i == 0 || i == size - 1 || j == 0 || j == size - 1 || i % 2 == 0 || j % 2 == 0)
                     {
-                        _tile[i, j] = TileType.Wall;
+                        Tile[i, j] = TileType.Wall;
                     }
                     else
                     {
-                        _tile[i, j] = TileType.Empty;
+                        Tile[i, j] = TileType.Empty;
                     }
                 }
             }
 
             // 랜덤으로 길 뚫기. 점 중 아무거나 하나 선택해서 길 뚫기 진행.
             Random rnd = new Random();
-            for (int i = 0; i < _size; i++)
+            for (int i = 0; i < size; i++)
             {
                 int cnt = 1;
-                for (int j = 0; j < _size; j++)
+                for (int j = 0; j < size; j++)
                 {
                     // 첫 행 열은 모두 벽이 되도록
                     if (i % 2 == 0 || j % 2 == 0)
@@ -59,14 +69,14 @@ namespace Maze
                     // 랜덤으로 점 중 아무 점을 선택하여 뚫리도록 설정. & 테두리 뚫리지 않도록 설정
                     if (rnd.Next(0, 2) == 0)
                     {
-                        if(j != _size-1)
-                            _tile[i, j + 1] = TileType.Empty;
+                        if(j != size-1)
+                            Tile[i, j + 1] = TileType.Empty;
                         cnt++;
                     }
                     else {
                         int rndIndex = rnd.Next(0, cnt);
-                        if(i != _size - 1)
-                            _tile[i + 1, j-rndIndex*2] = TileType.Empty;
+                        if(i != size - 1)
+                            Tile[i + 1, j-rndIndex*2] = TileType.Empty;
                         cnt = 1;
                     }
                 }
@@ -77,50 +87,50 @@ namespace Maze
         {
 
             // 테두리 및 짝수번째 막아버리는 작업
-            for (int i = 0; i < _size; i++)
+            for (int i = 0; i < size; i++)
             {
-                for (int j = 0; j < _size; j++)
+                for (int j = 0; j < size; j++)
                 {
-                    if (i == 0 || i == _size - 1 || j == 0 || j == _size - 1 || i % 2 == 0 || j % 2 == 0)
+                    if (i == 0 || i == size - 1 || j == 0 || j == size - 1 || i % 2 == 0 || j % 2 == 0)
                     {
-                        _tile[i, j] = TileType.Wall;
+                        Tile[i, j] = TileType.Wall;
                     }
                     else
                     {
-                        _tile[i, j] = TileType.Empty;
+                        Tile[i, j] = TileType.Empty;
                     }
                 }
             }
 
             // 랜덤으로 길 뚫기. 알고리즘이 단순할 수록 단점이 존재.
             Random rnd = new Random();
-            for (int i = 0; i < _size; i++)
+            for (int i = 0; i < size; i++)
             {
-                for (int j = 0; j < _size; j++)
+                for (int j = 0; j < size; j++)
                 {
                     // 첫 행 열은 모두 벽이 되도록
                     if (i % 2 == 0 || j % 2 == 0)
                         continue;
 
                     // 외곽에 있는 애들은 무조건 한 방향으로만 가도록 설정 => 마지막 -1 라인이 뚫릴 수 밖에.
-                    if (j == _size - 2)
+                    if (j == size - 2)
                     {
-                        _tile[i + 1, j] = TileType.Empty;
+                        Tile[i + 1, j] = TileType.Empty;
                         continue;
                     }
 
-                    if (i == _size - 2)
+                    if (i == size - 2)
                     {
-                        _tile[i, j + 1] = TileType.Empty;
+                        Tile[i, j + 1] = TileType.Empty;
                         continue;
                     }
 
                     // 랜덤으로 한 쪽 방향으로 뚫리도록 설정.
                     if (rnd.Next(0, 2) == 0)
                     {
-                        _tile[i, j + 1] = TileType.Empty;
+                        Tile[i, j + 1] = TileType.Empty;
                     }
-                    else { _tile[i + 1, j] = TileType.Empty; }
+                    else { Tile[i + 1, j] = TileType.Empty; }
                 }
             }
         }
@@ -128,12 +138,23 @@ namespace Maze
         public void Render()
         {
             ConsoleColor prevColor = Console.ForegroundColor;
-            for (int i = 0; i < _size; i++)
+            for (int i = 0; i < size; i++)
             {
-                for (int j = 0; j < _size; j++)
+                for (int j = 0; j < size; j++)
                 {
-
-                    Console.ForegroundColor = GetTileColor(_tile[i, j]);
+                    // player의 좌표를 받아 조건문으로 처리한다.
+                    // play의 좌표는 Player class에서 처리
+                    if(i == _player.PosY && j == _player.PosX)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                    }else if(i == DestY && j == DestX )
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = GetTileColor(Tile[i, j]);
+                    }
                     Console.Write(CIRCLE);
                 }
                 Console.WriteLine();
--- Program.cs
+++ Program.cs
@@ -33,12 +33,32 @@ namespace Maze
         static void Main(string[] args)
         {
             Board board = new Board(); //같은 namespace에 있어야됨. 패키지 느낌
-            board.Initialize(25);
+            Player player = new Player();
+            player.Initialize(1, 1, board);
+            board.Initialize(25, player);
 
             Console.CursorVisible = false;
 
+            const int WAIT_TICK = 1000 / 30;
+            int lastTick = 0;
+
             while (true)
             {
+                #region 프레임관리
+                // 만약 경과한 시간이 1/30초보다 작다면
+                int currentTick = System.Environment.TickCount;
+                if (currentTick - lastTick < WAIT_TICK)
+                    continue;
+                int deltaTick = currentTick - lastTick;
+                lastTick = currentTick;
+                #endregion
+                
+                // 입력
+
+                //로직
+                player.Update(deltaTick);
+
+                //렌더링
                 Console.SetCursorPosition(0, 0);
                 board.Render();
             }
